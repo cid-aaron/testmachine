74c75f75beaf83592b078dbcdf54d91669f19c1b
David R. MacIver <david@drmaciver.com>
2014-01-09 09:29:03 +0000

    oops correct output for that

--- testmachine/examples/commutativeints.py
+++ testmachine/examples/commutativeints.py
@@ -3,11 +3,7 @@ This attempts to show that integer addition is not commutative. Since integer
 addition is commutative, it will not have much luck.
 
 Example output:
-    t1 = 0.11945064104636571
-    t2 = t1 / t1
-    t3 = 0.16278913131835504
-    t4 = 0.6323432862008465
-    assert associative_add(t4, t3, t2)
+    Unable to find a failing program of length <= 200 after 500 iterations
 """
 
 from testmachine import TestMachine
