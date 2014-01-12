59ecb6befd40644293c4e3e4c9bbd41cacda560f
David R. MacIver <david@drmaciver.com>
2014-01-12 11:11:11 +0000

    0.0.4 release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if sys.version_info >= (3,):
 
 setup(
     name='testmachine',
-    version='0.0.3',
+    version='0.0.4',
     author='David R. MacIver',
     author_email='david@drmaciver.com',
     packages=['testmachine'],
